a840be410d943c1c129dee524db5d920f6157f75
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-08-03 05:50:37 +0600

    my-interaction page added

---
+++ src/app/my-interactions/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyInteractionPage = () => {
+    return (
+        <div>
+            <h2>My Interactions</h2>
+        </div>
+    );
+};
+
+export default MyInteractionPage;
